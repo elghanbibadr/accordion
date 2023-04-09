4f289bab12f257e7a87675890de641c3340c8a77
badr ghanbi <bghanbi50@gmail.com>
2023-04-09 20:05:15 +0000

    add the minus icon

--- accordion.js
+++ accordion.js
@@ -1,21 +1,36 @@
+
+
+
 let accordionItems = document.querySelectorAll('.accordion-item');
 
 accordionItems.forEach(function(item) {
-  let header = item.querySelector('.accordion-header');
+  let header = item.querySelector('.header-wrraper');
   let content = item.querySelector('.accordion-content');
+  let plusIcon = header.querySelector('.plus-icon');
+  let minusIcon = header.querySelector('.minus-icon');
 
   header.addEventListener('click', function() {
     if (item.classList.contains('active')) {
+      // Collapse the active item
       item.classList.remove('active');
-      content.style.display = 'none';
+      plusIcon.classList.remove('hidden');
+      minusIcon.classList.add('hidden');
+      content.style.maxHeight = null;
     } else {
-      accordionItems.forEach(function(item) {
-        item.classList.remove('active');
-        item.querySelector('.accordion-content').style.display = 'none';
-      });
+      // Collapse any active item
+      let activeItem = document.querySelector('.accordion-item.active');
+      if (activeItem) {
+        activeItem.classList.remove('active');
+        activeItem.querySelector('.plus-icon').classList.remove('hidden');
+        activeItem.querySelector('.minus-icon').classList.add('hidden');
+        activeItem.querySelector('.accordion-content').style.maxHeight = null;
+      }
 
+      // Expand the clicked item
       item.classList.add('active');
-      content.style.display = 'block';
+      plusIcon.classList.add('hidden');
+      minusIcon.classList.remove('hidden');
+      content.style.maxHeight = content.scrollHeight + 'px';
     }
   });
 });
